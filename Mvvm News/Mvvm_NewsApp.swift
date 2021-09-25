458c9c74eaf378e8b054c615e783a9102b1fbc0a
aashiq <ashik.cse.developer@gmail.com>
2021-09-26 00:18:33 +0600

    Initial Commit

---
+++ Mvvm News/Mvvm_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mvvm_NewsApp.swift
+//  Mvvm News
+//
+//  Created by Aashiq on 26/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Mvvm_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
